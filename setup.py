eaf92883843a8ac01250c5338db1d46c701792fe
szymanskir <ryszard.szymanski@outlook.com>
2019-05-20 19:47:15 +0200

    Init project

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='rnnhearer',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Speech recognition project for Deep Learning classes at the Warsaw University of Technology',
+    author='Paweł Rzepiński, Ryszard Szymański',
+    license='MIT',
+)
