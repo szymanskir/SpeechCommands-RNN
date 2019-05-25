a5bb1607e3629d0fe83f5e48d4dcf08b3bd275eb
szymanskir <ryszard.szymanski@outlook.com>
2019-05-25 16:02:06 +0200

    Make sample RNN smaller

--- rnnhearer/networks/sample_rnn.py
+++ rnnhearer/networks/sample_rnn.py
@@ -4,13 +4,7 @@ from typing import Tuple
 
 def create_sample_rnn(input_shape: Tuple[int], num_classes):
     model = models.Sequential()
-    model.add(
-        layers.CuDNNLSTM(
-            64, input_shape=input_shape, return_sequences=True
-        )
-    )
-    model.add(layers.CuDNNLSTM(64, return_sequences=True))
-    model.add(layers.CuDNNLSTM(64))
+    model.add(layers.CuDNNLSTM(32, input_shape=input_shape))
     model.add(layers.Dense(num_classes, activation="softmax"))
 
     return model
