98b353dc94ee2a5aacb8995cd6095cdb3d949a7d
szymanskir <ryszard.szymanski@outlook.com>
2019-05-28 01:23:52 +0200

    Add signal resampling before computing the spectrogram

--- rnnhearer/networks/audio_representation.py
+++ rnnhearer/networks/audio_representation.py
@@ -2,7 +2,7 @@ import numpy as np
 from abc import ABCMeta, abstractmethod
 from enum import auto
 from python_speech_features import mfcc
-from scipy.signal import spectrogram
+from scipy.signal import resample, spectrogram
 from strenum import StrEnum
 from typing import List, Tuple
 
@@ -24,8 +24,9 @@ class SpectrogramAudioRepresentationConverter(IAudioRepresentationConverter):
     def convert_audio_signal(
         self, audio_samples: List[Tuple[np.ndarray, int]]
     ) -> List[np.ndarray]:
+        sr = 8000
         return [
-            spectrogram(x=audio_sample[1], fs=audio_sample[0])[2]
+            spectrogram(x=resample(audio_sample[1], sr))[2]
             for audio_sample in audio_samples
         ]
 
